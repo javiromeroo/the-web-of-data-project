fd76633cde90f448d3873171a4aa2dc86f14fe4e
ElVichoSiu <vthielemunoz@gmail.com>
2024-11-25 17:41:45 -0300

    FEAT: Se arregla el procesamiento del neighbourhood_group de los Restaurantes

--- Google_maps_to_restaurant.py
+++ Google_maps_to_restaurant.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import re
 
-df = pd.read_csv('google_maps_restaurants(cleaned).csv')
+# Leer el archivo CSV
+df = pd.read_csv("google_maps_restaurants(cleaned).csv")
 
-df['neighbourhood_group'] = df['Address'].str.split(',').str[1].str.strip()
+# Función para extraer el neighbourhood
+def extract_neighbourhood(address):
+    # Usar regex para capturar la penúltima sección antes de la última coma
+    match = re.search(r',\s*([^,]+)\s*,\s*\w{2}\s*\d{5}', address)
+    if match:
+        return match.group(1).strip()
+    return None
+
+# Crear una nueva columna 'neighbourhood' basada en la columna 'Address'
+df['neighbourhood_group'] = df['Address'].apply(extract_neighbourhood)
 
 df.rename(columns={'Price Category': 'Price_Category'}, inplace=True)
 
-df.to_csv('Restaurant.csv', index=False)
+# Guardar el CSV con la nueva columna
+df.to_csv("Restaurant.csv", index=False)
